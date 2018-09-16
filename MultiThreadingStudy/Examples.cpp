9569b5c36f0ce4587b928a1fedce61c167c15c51
Zahit Ozcan <mzahit29@gmail.com>
2018-09-16 20:44:00 +0300

    - Parent to child, and child to parent thread communication via async, promise, future

--- MultiThreadingStudy/Examples.cpp
+++ MultiThreadingStudy/Examples.cpp
@@ -2,6 +2,7 @@
 #include "Examples.h"
 #include <queue>
 #include <mutex>
+#include <future>
 #include <iostream>
 
 using namespace std;
@@ -11,6 +12,64 @@ queue<int> q;
 mutex mq;
 
 condition_variable cond;
+
+
+void Examples::simple_synchronization_over_mutex()
+{
+	// Mutex for cout's synchronization in threads
+	mutex m;
+
+	thread t1{ [&m]()
+	{
+		lock_guard<mutex> lg(m);
+		cout << "[" << this_thread::get_id() << "] Inside thread" << endl;
+		this_thread::sleep_for(chrono::seconds(5));
+	} };
+	thread t2{ [&m]()
+	{
+		lock_guard<mutex> lg(m);
+		cout << "[" << this_thread::get_id() << "] Inside thread" << endl;
+	} };
+
+	t1.join();
+	t2.join();
+}
+
+void Examples::simple_synchronization_over_mutex_unique_lock()
+{
+	// Mutex for cout's synchronization in threads
+	mutex m;
+
+	thread t1{ [&m]()
+	{
+		unique_lock<mutex> ul(m, defer_lock);
+		ul.lock();
+		cout << "[" << this_thread::get_id() << "] Inside thread" << endl;
+		ul.unlock(); // Release lock not to make other threads wait on cout's mutex
+
+		this_thread::sleep_for(chrono::seconds(5));
+	}
+	};
+
+	thread t2{ [&m]()
+	{
+		unique_lock<mutex> ul(m, defer_lock);
+		ul.lock();
+		cout << "[" << this_thread::get_id() << "] Inside thread" << endl;
+		ul.unlock();
+
+		// Do some other stuff which doesn't require shared resource (cout) usage
+
+		ul.lock();
+		cout << "[" << this_thread::get_id() << "] Locking mutex to use cout after some processing in thread again." << endl;
+		ul.unlock();
+	}
+	};
+
+
+	t1.join();
+	t2.join();
+}
 
 void produce(queue<int>& q)
 {
@@ -102,60 +161,96 @@ void Examples::producer_consumer_problem_with_conditional_variable()
 	t2.join();
 }
 
-void Examples::simple_synchronization_over_mutex()
+mutex mutex_fact;
+condition_variable cond_fact;
+// Lock the mutex_fact to synchronize result parameter with parent
+void factorial_with_ref_result_parameter(int x, int& result)
 {
-	// Mutex for cout's synchronization in threads
-	mutex m;
+	cout << "[" << this_thread::get_id() << "] Calculating factorial of " << x << endl;
 
-	thread t1{ [&m]()
+	unique_lock<mutex> ul(mutex_fact);
+	result = 1;
+	for (int i = 2; i <= x; ++i)
 	{
-		lock_guard<mutex> lg(m);
-		cout << "[" << this_thread::get_id() << "] Inside thread" << endl;
-		this_thread::sleep_for(chrono::seconds(5));
-	} };
-	thread t2{ [&m]()
-	{
-		lock_guard<mutex> lg(m); 
-		cout << "[" << this_thread::get_id() << "] Inside thread" << endl;
-	} };
-
-	t1.join();
-	t2.join();
+		result *= i;
+	}
+	ul.unlock();
 }
 
-void Examples::simple_synchronization_over_mutex_unique_lock()
+// Instead of using async(..) and using the returned future to get the result from the child thread,
+// we use the std::ref(result) being passed to the child thread which fills the calculated result
+// and main thread can see the result.
+void Examples::factorial_with_conditional_wait()
 {
-	// Mutex for cout's synchronization in threads
-	mutex m;
-
-	thread t1 { [&m]()
+	int result{};
+	thread t1{ factorial_with_ref_result_parameter, 4, std::ref(result)};
+	unique_lock<mutex> ul(mutex_fact);
+	//cond.wait(ul, [&result]()  // Waits until notify_one(), spurious wake up may happen not guaranteed, this is why predicate is req.
+	cond.wait_for(ul, chrono::seconds(2), [&result]()  // Waits for 2 seconds, then if not notified during this time, wakes up by itself.
 	{
-		unique_lock<mutex> ul(m, defer_lock);
-		ul.lock();
-		cout << "[" << this_thread::get_id() << "] Inside thread" << endl;
-		ul.unlock(); // Release lock not to make other threads wait on cout's mutex
-
-		this_thread::sleep_for(chrono::seconds(5));
-	}
-	};
-
-	thread t2 { [&m]()
-	{
-		unique_lock<mutex> ul(m, defer_lock);
-		ul.lock();
-		cout << "[" << this_thread::get_id() << "] Inside thread" << endl;
-		ul.unlock();
-
-		// Do some other stuff which doesn't require shared resource (cout) usage
-
-		ul.lock();
-		cout << "[" << this_thread::get_id() << "] Locking mutex to use cout after some processing in thread again." << endl;
-		ul.unlock();
-	}
-	};
-
+		cout << "Inside condition predicate" << endl;
+		cout << "Result in parent: " << result << endl;
+		return result != 0;
+	});
+	cout << "Factorial result after factorial_with_ref_result_parameter thread joined parent: " << result << endl;
+	ul.unlock();
 
 	t1.join();
-	t2.join();
 }
 
+// This function is used with async(..) to return a value of future<int>
+int factorial_to_return_a_future(int x)
+{
+	cout << "[" << this_thread::get_id() << "] Calculating factorial of " << x << endl;
+
+	int result{ 1 };
+	for (int i = 1; i <= x; ++i)
+	{
+		result *= i;
+	}
+	return result;
+}
+
+// The thread that executes this function will be waiting for a future value.
+// This future value will be provided by the parent thread with promise.set_value(..)
+// This accomplishes the parent to child thread communication
+int factorial_waiting_for_promise(future<int>& fut)
+{
+	int x = fut.get();
+	cout << "[" << this_thread::get_id() << "] Calculating factorial of " << x << endl;
+
+	int result{ 1 };
+	for (int i = 1; i <= x; ++i)
+	{
+		result *= i;
+	}
+	return result;
+}
+
+void Examples::factorial_async_future_promise()
+{
+	// Child thread sending data to parent thread with async(..)
+	// Makes factorial_to_return_a_future run on the parent thread when fut.get() is called
+	//future<int> fut = async(launch::deferred, factorial_to_return_a_future, 4);  
+	// Makes factorial_to_return_a_future run on the child thread as soon as this async func call is made
+	future<int> fut = async(launch::async, factorial_to_return_a_future, 4);  
+
+	this_thread::sleep_for(chrono::seconds(1));
+	cout << "[" << this_thread::get_id() << "] Factorial result calculated from child thread: " << fut.get() << endl;
+
+
+	// Parent thread sending data to child thread. In this case the value of factorial that is to be calculated
+	promise<int> p;
+	future<int> fut2 = p.get_future();
+
+	future<int> fut3 = async(launch::async, factorial_waiting_for_promise, std::ref(fut2));
+	this_thread::sleep_for(chrono::seconds(2));
+	// Fullfill your promise, if you don't fullfil it then child thread will be waiting on fut.get() to get the x
+	// to calculate the factorial for
+	p.set_value(3);
+	cout << "[" << this_thread::get_id() << "] Factorial result calculated from child thread" 
+		<< " that was waiting for promise to be fullfilled from parent: " << fut3.get() << endl;
+
+
+
+}
--- MultiThreadingStudy/Examples.h
+++ MultiThreadingStudy/Examples.h
@@ -11,5 +11,7 @@ public:
 	static void simple_synchronization_over_mutex_unique_lock();
 	static void producer_consumer_problem_with_busy_waiting();
 	static void producer_consumer_problem_with_conditional_variable();
+	static void factorial_with_conditional_wait();
+	static void factorial_async_future_promise();
 };
 
